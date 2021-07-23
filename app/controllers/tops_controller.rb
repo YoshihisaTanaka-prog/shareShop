4ecf9bc3b410988b1ae4c7b9356ef0b43bcb2c87
TanakaYoshihisa <1sc16346k@gmail.com>
2021-07-23 20:49:12 +0900

    言語選択機能

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,9 +2,15 @@ class TopsController < ApplicationController
 
     def index
         if user_signed_in?
-            @page_name = "signedIn"
+            @page_name = "IndexSignedIn"
+            # 各言語に対応させる。
+            @page_name = params[:lang] + @page_name
         else
-            @page_name = "signedOut"
+            if params[:lang].blank?
+                @page_name = "jpIndexSignedOut"
+            else
+                @page_name = params[:lang] + "IndexSignedOut"
+            end
         end
     end
 end
