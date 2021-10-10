46d6fc0cddfa47f973715e16f807c5e14878b9d5
YoshihisaTanaka-prog <72920523+YoshihisaTanaka-prog@users.noreply.github.com>
2021-10-10 10:50:37 +0900

    言語選択の効率化
    言語選択用の仕組みを実装するために必要なコードの量を減らす方法を思いついたので途中まで実装しました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,42 @@
 class ApplicationController < ActionController::Base
+    # アクションが実行される前に行う処理
+    before_action :def_at_language
+
+    # @languageを定義するためのメソッド
+    def def_at_language
+        if user_signed_in?
+            # ユーザーがサインインしている場合
+            # そのユーザーの設定している言語を反映
+            @language = current_user.language
+        elsif session[:language]
+            # サインインしていない場合
+            # 言語のセッションが埋められている場合
+            # 言語のセッションに保存されている言語を反映
+            @language = session[:language]
+        else
+            # サインインしていない場合
+            # 言語のセッションが埋められていない場合
+            # デフォルトの日本語を反映させ、言語のセッションに「日本語」という情報を保存
+            session[:language] = 'Japanese'
+            @language = 'Japanese'
+        end
+        case @language
+        when 'Japanese'
+            @language_option = [['言語・Language・语言','#'],['English'],[]]
+        when 'English'
+            @language_option = [['言語・Language・语言','#'],[],[]]
+        when 'Chinese'
+            @language_option = [['言語・Language・语言','#'],[],[]]
+        end
+    end
+
+    # devise用のカラムを追加するために必要
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    def configure_permitted_parameters
+        # サインインする時に「name」、「room」、「language」カラムをいじることを許可
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :room, :language])
+        # ユーザー情報を編集する時に「name」、「room」カラムをいじることを許可
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name, :room])
+    end
+
 end
