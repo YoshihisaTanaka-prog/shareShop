1db9bff3b6f1219095a7187cd414d3a7d82d5a93
YoshihisaTanaka-prog <72920523+YoshihisaTanaka-prog@users.noreply.github.com>
2021-10-10 12:18:26 +0900

    言語選択の効率化
    ログアウト時に言語の選択状態が失われることを除けばうまくいくようになったはずです。

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   #devise_for :users
-  Rails.application.routes.draw do
-    devise_for :users, controllers: {
-      sessions: 'users/sessions',
-      registrations: 'users/registrations'
-    }
-  end
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # 大元のページ
   root 'tops#index'
+  # 言語選択用のルート
+  get 'language', to: 'tops#set_language'
+
+
 end
