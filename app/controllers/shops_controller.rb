a6fd0511010e6f1a121596caffba3c56fc6a319a
Wakatose <86833790+Wakatose@users.noreply.github.com>
2021-10-20 19:54:32 +0900

    店の一覧ページ少し作成

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,15 +1,20 @@
 class ShopsController < ApplicationController
+  # 今まで投稿された店の名前を全部見れる
   def index
-    @shoplist = ["ion","gyoumusuper","trial"] 
+    @shoplist = Shop.all
 
   end
+
+  #new.html.erbのためなので何も書かない
   def new
-
+    
   end
+
+# 投稿した店をshopテーブルに追加する
   def create
     # フォームから送信されたデータを受け取り、保存する処理を追加してください
-    @shops = Shop.new(japanese_name: params[:content])
-    @shops.save
+    @shop = Shop.new(japanese_name: params[:content])
+    @shop.save
     redirect_to("/shops")
   end
 end
