0fde2dda273c719d66395d740540667834a6ecda
TanakaYoshihisa <1sc16346k@gmail.com>
2021-11-10 19:08:44 +0900

    カテゴリーにメソッドを追加

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,27 @@
 class Category < ApplicationRecord
+    def sub_categories
+        ret = []
+        sub = Category.where(parent_id: self.id)
+        sub.each do |s|
+            ret.push s
+        end
+        sub.each do |s|
+            ret.push s.sub_categories
+        end
+        ret.flatten!
+        return ret 
+    end
+    
+    def parents
+        ret = []
+        if self.parent_id == 0
+            return ret
+        else
+            parent = Category.find(self.parent_id)
+            ret.push parent
+            ret.push parent.parents
+            ret.flatten!
+            return ret
+        end
+    end
 end
